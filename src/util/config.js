cdd163f4e7a747849c9cbd0a4d9192a722ea881a
RAJESH2132 <rajeshnagurla@gmail.com>
2026-04-16 09:59:00 +0530

    implemented profile upload functionality

---
+++ src/util/config.js
@@ -0,0 +1,6 @@
+const config = {
+    baseUrl : import.meta.env.VITE_BASE_URL,
+    cloudName : import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
+    uploadPresent : import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET 
+}
+ export default config;
